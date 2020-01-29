e67a70ac63d7ac33f6bc6287c09671157ab96f20
Theodosis Melidis <melidisteo@gmail.com>
2020-01-30 01:32:50 +0200

    fix: add test for dublicate react

--- src/components/app/WithPropsApp.js
+++ src/components/app/WithPropsApp.js
@@ -1,9 +1,19 @@
-import useGameStartedModel from "models/game-started/useGameStartedModel";
+import { useGameStartedModel } from "models/game-started";
 
 import React from "react";
 
 const WithPropsApp = Component => {
+  console.log("*************************");
+
+  require("react-dom");
+  window.React2 = require("react");
+  console.log(window.React1 === window.React2);
+
   const { started, startGame, notStartGame } = useGameStartedModel();
+
+  console.log("####################");
+  console.log(started);
+  console.log(startGame);
 
   return (
     <Component
